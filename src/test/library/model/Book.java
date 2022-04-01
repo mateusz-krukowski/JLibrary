b664f915ada165c01d272e3d37e291ec98d1b054
Mateusz <matwoz2001@icloud.com>
2022-04-01 23:56:23 +0200

    0.4

--- src/test/library/model/Book.java
+++ src/test/library/model/Book.java
@@ -1,12 +1,60 @@
 package test.library.model;
 
 public class Book {
-    String title;
-    String author;
-    int releaseDate;
-    int pages;
-    String publisher;
-    String isbn;
+   private String title;
+   private String author;
+   private int releaseDate;
+   private int pages;
+   private String publisher;
+   private String isbn;
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public void setAuthor(String author) {
+        this.author = author;
+    }
+
+    public int getReleaseDate() {
+        return releaseDate;
+    }
+
+    public void setReleaseDate(int releaseDate) {
+        this.releaseDate = releaseDate;
+    }
+
+    public int getPages() {
+        return pages;
+    }
+
+    public void setPages(int pages) {
+        this.pages = pages;
+    }
+
+    public String getPublisher() {
+        return publisher;
+    }
+
+    public void setPublisher(String publisher) {
+        this.publisher = publisher;
+    }
+
+    public String getIsbn() {
+        return isbn;
+    }
+
+    public void setIsbn(String isbn) {
+        this.isbn = isbn;
+    }
 
     public Book(String title, String author, int releaseDate, int pages, String publisher, String isbn) {
         this.title = title;
