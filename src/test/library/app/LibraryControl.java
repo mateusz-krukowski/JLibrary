d9ee463bcf97684f4475b1792dec65ab1f89432e
Mateusz <matwoz2001@icloud.com>
2022-04-05 14:21:08 +0200

    1.3

--- src/test/library/app/LibraryControl.java
+++ src/test/library/app/LibraryControl.java
@@ -85,4 +85,42 @@ public class LibraryControl {
     private void printMagazines() {
         printer.printMagazines(library.getPublications());
     }
+//======================================================================//
+    private enum Option {
+        EXIT(0,"Wyjście z programu"),
+        ADD_BOOK(1, "Dodaj nową książkę"),
+        ADD_MAGAZINE(2,"Dodaj nowy magazyn"),
+        PRINT_BOOKS(3,"Wyświetl dostępne książki"),
+        PRINT_MAGAZINES(4,"Wyświetl dostępne magazyny");
+
+        private final int value;
+        private final String description;
+
+        Option(int value, String description) {
+            this.value = value;
+            this.description = description;
+        }
+
+        public int getValue() {
+            return value;
+        }
+
+        public String getDescription() {
+            return description;
+        }
+
+        @Override
+        public String toString() {
+            return value + " - " + description;
+        }
+
+        static Option createFromint(int i) {
+            try {
+                return Option.values()[i];
+            } catch (ArrayIndexOutOfBoundsException e){
+                throw new NoSuchOptionException("Brak opcji o ID "+ i);
+            }
+        }
+    }
+
 }
